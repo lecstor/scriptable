456692ab15a8e5e03dc13f6442c9ea6867f924aa
Jason Galea <jason@lecstor.com>
2017-08-19 23:50:44 +1000

    refactor evaluate

--- safejs/evaluate.js
+++ safejs/evaluate.js
@@ -54,64 +54,71 @@ function applyOp(op, a, b) {
   throw new Error("Can't apply operator " + op);
 }
 
-function evaluate(exp, env) {
-  switch (exp.type) {
-    case "num":
-    case "str":
-    case "bool":
-      return exp.value;
-
-    case "var":
-      return env.get(exp.value);
-
-    case "assign":
-      if (exp.left.type !== "var") {
-        throw new Error("Cannot assign to " + JSON.stringify(exp.left));
-      }
-      return env.set(exp.left.value, evaluate(exp.right, env));
-
-    case "binary":
-      return applyOp(
-        exp.operator,
-        evaluate(exp.left, env),
-        evaluate(exp.right, env)
-      );
-
-    case "lambda":
-      return makeLambda(env, exp);
-
-    case "if":
-      var cond = evaluate(exp.cond, env);
-      if (cond !== false) {
-        return evaluate(exp.then, env);
-      }
-      return exp.else ? evaluate(exp.else, env) : false;
-
-    case "forEach":
-      var list = evaluate(exp.list, env);
-      var feFunc = evaluate(exp.func, env);
-      list.forEach(feFunc);
-      return true;
-
-    case "prog":
-      var val = false;
-      exp.prog.forEach(function(exp) {
-        val = evaluate(exp, env);
-      });
-      return val;
-
-    case "call":
-      var func = evaluate(exp.func, env);
-      return func.apply(
-        null,
-        exp.args.map(function(arg) {
-          return evaluate(arg, env);
-        })
-      );
-
-    default:
-      throw new Error("I don't know how to evaluate " + exp.type);
+const evalOps = {
+  num(exp, env) {
+    return exp.value;
+  },
+  str(exp, env) {
+    return exp.value;
+  },
+  bool(exp, env) {
+    return exp.value;
+  },
+  assign(exp, env) {
+    if (exp.left.type !== "var") {
+      throw new Error("Cannot assign to " + JSON.stringify(exp.left));
+    }
+    return env.set(exp.left.value, evaluate(exp.right, env));
+  },
+  binary(exp, env) {
+    return applyOp(
+      exp.operator,
+      evaluate(exp.left, env),
+      evaluate(exp.right, env)
+    );
+  },
+  lambda(exp, env) {
+    return makeLambda(env, exp);
+  },
+  prog(exp, env) {
+    var val = false;
+    exp.prog.forEach(function(exp) {
+      val = evaluate(exp, env);
+    });
+    return val;
+  },
+  call(exp, env) {
+    var func = evaluate(exp.func, env);
+    return func.apply(
+      null,
+      exp.args.map(function(arg) {
+        return evaluate(arg, env);
+      })
+    );
+  },
+  if: (exp, env) => {
+    var cond = evaluate(exp.cond, env);
+    if (cond !== false) {
+      return evaluate(exp.then, env);
+    }
+    return exp.else ? evaluate(exp.else, env) : false;
+  },
+  forEach: (exp, env) => {
+    var list = evaluate(exp.list, env);
+    var feFunc = evaluate(exp.func, env);
+    list.forEach(feFunc);
+    return true;
+  },
+  var: (exp, env) => {
+    return env.get(exp.value);
   }
+};
+
+function evaluate(exp, env) {
+  if (evalOps[exp.type]) {
+    return evalOps[exp.type](exp, env);
+  }
+  throw new Error("I don't know how to evaluate " + exp.type);
 }
 
 module.exports = evaluate;
